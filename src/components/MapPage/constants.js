7d4b4ea7ba195f945ea43d515549fc391a8f7dda
jaime-varela <varela.jaime01@gmail.com>
2020-10-05 01:57:27 -0400

    new icon page

--- src/components/MapPage/constants.js
+++ src/components/MapPage/constants.js
@@ -1,3 +1,5 @@
+//TODO: move these to public URLS
 export const iconURLS = {
-    blueIcon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+    blueIcon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+    redIcon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
 }
--- src/components/QueryForm/QueryForm.js
+++ src/components/QueryForm/QueryForm.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import QueryText from './QueryText'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 import { cases } from '../../constants'
 import { placeHolder , formIDS } from './queryConstants'
+import { iconURLS } from '../MapPage/constants'
 
 export class QueryForm extends Component {
 
@@ -20,12 +24,26 @@ export class QueryForm extends Component {
             <Form onSubmit={this.onSubmit}>
               <Form.Group controlId="formPointOfInterest1">
                 <Form.Label>Find </Form.Label>
-                <Form.Control id={formIDS.poi1} type="text" placeholder={placeHolder.poi1} />
+                <Row>
+                  <Col>
+                    <Form.Control id={formIDS.poi1} type="text" placeholder={placeHolder.poi1} />
+                  </Col>
+                  <Col sm={2}>
+                    <Image src={iconURLS.redIcon} roundedCircle />
+                  </Col>
+                </Row>
               </Form.Group>
 
               <Form.Group controlId="formPointOfInterest2">
                 <Form.Label>And</Form.Label>
-                <Form.Control id={formIDS.poi2} type="text" placeholder={placeHolder.poi2} />
+                <Row>
+                  <Col>
+                    <Form.Control id={formIDS.poi2} type="text" placeholder={placeHolder.poi2} />
+                  </Col>
+                  <Col sm={2}>
+                    <Image src={iconURLS.blueIcon} roundedCircle />
+                  </Col>
+                </Row>
               </Form.Group>
               <Form.Group controlId="formDistanceSpecification">
                 <Form.Label>Within</Form.Label>
