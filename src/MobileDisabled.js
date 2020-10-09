19d3c5fd2598aba3d4ef00685af474cf0a344fdb
jaime-varela <varela.jaime01@gmail.com>
2020-10-09 18:29:42 -0400

    added mobile blocker

---
+++ src/MobileDisabled.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div style={{textAlign: "center", padding: "30px"}}>
+            <p> This site does not support mobile, a mobile app is under development.
+            </p>
+        </div>
+    )
+}
