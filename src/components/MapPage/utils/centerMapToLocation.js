b6257cc79570a65b4efae722ad0f57323db9213a
jaime-varela <varela.jaime01@gmail.com>
2020-10-09 21:11:37 -0400

    geolocation fallback

--- src/components/MapPage/utils/centerMapToLocation.js
+++ src/components/MapPage/utils/centerMapToLocation.js
@@ -11,11 +11,24 @@ export default (map) =>
           map.setCenter(pos);
         },
         () => {
-            //TODO: handle bad current position request
-        }
-      );
-    } else {
-        // TODO: handle restrictions error
+          var ipRequest = new XMLHttpRequest();
+          ipRequest.open('GET', 'https://freegeoip.app/json/');
+          ipRequest.setRequestHeader('Accept', 'application/json');
+          ipRequest.onreadystatechange = () => {
+            let data = {};
+            if (ipRequest.readyState === 4) {
+              data = JSON.parse(ipRequest.responseText);
+              console.log(ipRequest);
+              console.log(ipRequest.responseText);
+              map.setCenter({lat: data.latitude,
+                 lng: data.longitude,
+              });
+            }
+          };
+          ipRequest.send();
+    });
+   } else {
+    console.log("Geolocation services are not supported by your web browser.");
     }
 
 }
