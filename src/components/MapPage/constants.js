9f9e2076c600ae852d6d74385343701474d95fe4
jaime-varela <varela.jaime01@gmail.com>
2020-10-10 00:31:42 -0400

    updated icons to https

--- src/components/MapPage/constants.js
+++ src/components/MapPage/constants.js
@@ -1,7 +1,7 @@
 //TODO: move these to public URLS
 export const iconURLS = {
-    blueIcon: "http://maps.google.com/mapfiles/ms/micons/blue.png",
-    redIcon: "http://maps.google.com/mapfiles/ms/micons/red.png",
+    blueIcon: "https://maps.google.com/mapfiles/ms/micons/blue.png",
+    redIcon: "https://maps.google.com/mapfiles/ms/micons/red.png",
 };
 
 export const infoWinClassNames = {
